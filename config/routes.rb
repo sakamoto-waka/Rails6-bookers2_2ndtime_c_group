d36b888f3fcf5a4a96cdfdddbbcdc9c88da48bd9
sakamoto-waka <wakanmhl@gmail.com>
2022-04-11 12:29:06 +0000

    [update] new routing for 'new/mail'+'send/mail'

--- config/routes.rb
+++ config/routes.rb
@@ -24,6 +24,8 @@ Rails.application.routes.draw do
     # resourcesのdestroyとかぶってるので名前を変えて新たなルーティング
     # ↑のはグループメンバーを消す、↓はグループを消す
     delete 'group_destroy' => 'groups#group_destroy'
+    get 'new/mail' => 'groups#new_mail'
+    get 'send/mail' => 'groups#send_mail'
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
