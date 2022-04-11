2bcb2531b13f85795c131a35cdcf9d22e21c8a01
sakamoto-waka <wakanmhl@gmail.com>
2022-04-11 12:07:30 +0000

    [install] gem 'dotenv-rails'

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -76,4 +76,18 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts << "b1f485c3591b46919cd7ee6a36e842b7.vfs.cloud9.us-east-1.amazonaws.com"
   config.hosts << "c7db326fda3f46239ea238ccc4c2eab4.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  
+  config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:                 587,
+    address:              'smtp.gmail.com',
+    domain:               'gmail.com',
+    user_name:            'メルアド',#Gmailのアドレス
+    password:             'アプリパスワード',
+    authentication:       'login',
+    enable_starttls_auto: true
+  }
+
 end
