6a2c3ce5b76fd060d38c48556071b7330fb07eae
sakamoto-waka <wakanmhl@gmail.com>
2022-04-11 09:00:09 +0000

    [update] routing for groups#join

--- config/routes.rb
+++ config/routes.rb
@@ -19,6 +19,8 @@ Rails.application.routes.draw do
     get 'followers' => 'relationships#followers', as: 'followers'
   end
   get "search" => "searches#search"
-  resources :groups, exept: :destroy
+  resources :groups, exept: :destroy do
+    get 'join' => 'groups#join'
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
